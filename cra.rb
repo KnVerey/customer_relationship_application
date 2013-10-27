03ded03a6b5b3fde207a77f214d86be3f57023b5
Katrina Verey <kn.verey@gmail.com>
2013-10-27 19:55:06 -0400

    Fixed bug where failed when loaded empty existing book, fully implemented book deletion

--- cra.rb
+++ cra.rb
@@ -35,7 +35,11 @@ class CRA
 			 		new_rolodex.insert_database_contact(id, first_name, last_name, email, note)
 			 		id_counter << id
 			 end
-			 new_rolodex.id_to_assign = (id_counter.sort.last + 1)
+			 if id_counter.empty? #empty contact book
+			 		new_rolodex.id_to_assign = 1
+			 else
+			 		new_rolodex.id_to_assign = (id_counter.sort.last + 1)
+			 end
 			 @rolodex_list << new_rolodex
 			 new_rolodex.sort_by_name
 		end
@@ -55,21 +59,59 @@ class CRA
 			puts "[#{selector}]: #{book.name}"
 			selector += 1
 		end
+		puts
 		puts "[#{selector}]: Add a new book" #incremented after last book
+		puts "[#{selector+1}]: Delete an existing book"
 		print "\nCHOICE: "
 		input = gets.chomp.to_i
 
-		if (1..(selector-1)).include? input
+		if (1..(selector-1)).include? input #within indices
 			open_rolodex(input-1) #i.e. index
 		elsif input == selector #i.e. add new
-				clear
-				puts cra_header("Add new contact book")
-				add_new_rolodex
+			clear
+			puts cra_header("Add new contact book")
+			add_new_rolodex
+		elsif input==(selector+1) #i.e. delete
+			clear
+			puts cra_header("Delete a contact book")
+			puts "Which book do you want to delete?"
+			delete_a_book
 		else
 			clear
 	 		puts cra_header("Choose contact book")
 	 		print "\nThat is not a valid choice. "
 	 		input = choose_rolodex
+		end
+	end
+
+	def self.delete_a_book
+		selector = 1 #0 cannot be a valid choice (string.to_i)
+		@rolodex_list.each do |book|
+			puts "[#{selector}]: #{book.name}"
+			selector += 1
+		end
+		puts "[#{selector}]: CANCEL"
+		print "\nCHOICE: "
+		input = gets.chomp.to_i
+
+		if (1..(selector-1)).include? input #within indices
+			@rolodex_list.delete_at(input-1) #i.e. index
+			clear
+			puts cra_header("Delete a contact book")
+			puts "Contact book successfully deleted. Press enter to continue."
+			gets
+			clear
+			puts cra_header("Choose contact book")
+			choose_rolodex
+		elsif selector==input #i.e. cancel
+			clear
+			puts cra_header("Choose contact book")
+ 			choose_rolodex
+		else
+			clear
+			puts cra_header("Delete a contact book")
+			puts "\n That is not a valid choice. "
+			input = delete_a_book
 		end
 	end
 
